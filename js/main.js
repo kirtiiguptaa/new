931e495734a5283d459d784c3b810c3e7a8d3977
Kirti Gupta <158804149+kirtiiguptaa@users.noreply.github.com>
2024-04-28 15:13:02 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -86,6 +86,31 @@ document.addEventListener("DOMContentLoaded", function() {
     },
   });
 
+
+  document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        function counter(id, start, end, duration){
+            let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration /  range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if(current == end){
+                    clearInterval(timer);
+                }
+            }, step);
+        }
+        counter("count1", 0, 50, 5000);
+        counter("count2", 0, 200, 7000);
+        counter("count3", 0, 120, 6000);
+    }, 4000);
+});
+
+
+
 (function ($) {
     "use stict";
     var count = 1;
